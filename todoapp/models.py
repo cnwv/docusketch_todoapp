ffadbba55c24b87cac588dbc9a677f15b31d57f4
cnwv <115669384+cnwv@users.noreply.github.com>
2023-12-02 21:47:37 +0400

    create model for tasks

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=124)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Task(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}'
